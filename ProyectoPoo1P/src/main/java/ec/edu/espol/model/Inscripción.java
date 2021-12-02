9221bc05fa7a89c6657146cd142f5697c67e2ee8
Sebastian Arguello <sebaescu@espol.edu.ec>
2021-12-01 22:32:09 -0500

    version 1

---
+++ ProyectoPoo1P/src/main/java/ec/edu/espol/model/Inscripción.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel Bejarano
+ */
+public class Inscripción {
+    private int id, idMascota, idConcurso;
+    private Mascota mascota;
+    private Concurso concurso;
+    private ArrayList<Evaluación> evaluaciones;
+    
+}
